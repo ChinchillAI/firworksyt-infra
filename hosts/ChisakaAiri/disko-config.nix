0a8ad84af401fabcc897a978f9c8b75b8a76c937
Firworks <74845631+Firworksyt@users.noreply.github.com>
2025-09-15 02:02:28 -0500

    Added gitignore anad fix for disko

--- hosts/ChisakaAiri/disko-config.nix
+++ hosts/ChisakaAiri/disko-config.nix
@@ -1,9 +1,7 @@
-# /hosts/ChisakaAiri/disko-config.nix
-{ disko, ... }:
-
+# THIS IS THE NEW, CORRECT VERSION
 {
-  imports = [ disko.nixosModules.disko ];
-
+  # The 'imports' line is not needed for the CLI tool and is handled
+  # by the main flake evaluation, so we can remove it.
   disko.devices = {
     disk.vda = {
       device = "/dev/vda";
@@ -11,7 +9,6 @@
       content = {
         type = "gpt";
         partitions = {
-          # EFI boot partition is still required for UEFI boot
           ESP = {
             size = "1G";
             type = "EF00";
@@ -21,41 +18,18 @@
               mountpoint = "/boot";
             };
           };
-          # ZFS partition takes up the rest of the disk
           zfs = {
             size = "100%";
             content = {
               type = "zfs_pool";
-              pool = "rpool"; # The name of our root pool
-              # ZFS datasets are defined here
+              pool = "rpool";
               datasets = {
-                # These are "blank" datasets that hold the mounted ones
-                # This allows for easier snapshotting and management
-                "root" = {
-                  type = "zfs_fs";
-                  mountpoint = "none";
-                };
-                "data" = {
-                  type = "zfs_fs";
-                  mountpoint = "none";
-                };
-                # The actual mounted datasets
-                "nixos" = {
-                  type = "zfs_fs";
-                  mountpoint = "/";
-                  postCreateHook = "zfs set atime=off rpool/nixos";
-                };
-                "nix" = {
-                  type = "zfs_fs";
-                  mountpoint = "/nix";
-                  postCreateHook = "zfs set atime=off rpool/nix";
-                };
-                "home" = {
-                  type = "zfs_fs";
-                  mountpoint = "/home";
-                };
+                "root" = { type = "zfs_fs"; mountpoint = "none"; };
+                "data" = { type = "zfs_fs"; mountpoint = "none"; };
+                "nixos" = { type = "zfs_fs"; mountpoint = "/"; postCreateHook = "zfs set atime=off rpool/nixos"; };
+                "nix" = { type = "zfs_fs"; mountpoint = "/nix"; postCreateHook = "zfs set atime=off rpool/nix"; };
+                "home" = { type = "zfs_fs"; mountpoint = "/home"; };
               };
-              # Set some sane default options for the entire pool
               rootFsOptions = {
                 compression = "lz4";
                 xattr = "sa";
